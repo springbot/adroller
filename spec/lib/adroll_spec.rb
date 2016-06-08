e00c03072ac3b2845b205db73ef1bad3ea3c6934
Serra Allgood <sallgood@springbot.com>
2016-06-08 13:15:28 -0400

    SB-4918: Modify adroll_spec

--- spec/lib/adroll_spec.rb
+++ spec/lib/adroll_spec.rb
@@ -4,23 +4,27 @@ class Dummy
   include AdRoll::Api
 end
 
-describe AdRoll::Api do
+fdescribe AdRoll::Api do
   subject { Dummy }
 
-  it 'should return username' do
-    expect(subject.user_name).to eq 'USERNAME'
-  end
+  describe 'when included' do
+    it 'should define user_name with attr_accessor' do
+      expect(subject.respond_to?(:user_name)).to be_truthy
+      expect(subject.respond_to?(:user_name=)).to be_truthy
+    end
 
-  it 'should return password' do
-    expect(subject.password).to eq 'PASSWORD'
-  end
+    it 'should define user_name with attr_accessor' do
+      expect(subject.respond_to?(:password)).to be_truthy
+      expect(subject.respond_to?(:password=)).to be_truthy
+    end
 
-  it 'should return organization eid' do
-    expect(subject.organization_eid).to eq 'ORG123XYZ'
-  end
+    it 'should define user_name with attr_accessor' do
+      expect(subject.respond_to?(:organization_eid)).to be_truthy
+      expect(subject.respond_to?(:organization_eid=)).to be_truthy
+    end
 
-  it 'should return the base url for api endpoints' do
-    expect(subject.base_url).to eq 'https://api.adroll.com/v1'
+    it 'should define self.base_url' do
+      expect(subject.base_url).to eq 'https://api.adroll.com/v1'
+    end
   end
-
 end
