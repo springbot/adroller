d3c4341a32c75a1e97b865c69a73303b2ac51020
Nikolas Davis <nikolasliamdavis@gmail.com>
2016-09-26 16:58:45 -0400

    SB-7604: Specify data source for client

--- lib/adroll/client.rb
+++ lib/adroll/client.rb
@@ -2,15 +2,21 @@ module AdRoll
   class Client
     attr_accessor :user_name, :password, :organization_eid, :debug
 
-    def initialize(user_name:, password:, organization_eid:, debug: false)
+    def initialize(user_name:, password:, organization_eid:, data_source: 'api', debug: false)
       @user_name = user_name
       @password = password
       @organization_eid = organization_eid
       @debug = debug
+      @data_source = data_source
     end
 
     def method_missing(meth, *args, &block)
+      case @data_source
+      when 'api'
       "AdRoll::Api::#{camelize(meth)}".constantize.new(client: self)
+      when 'uhura'
+      "AdRoll::Uhura::#{camelize(meth)}".constantize.new(client: self)
+      end
     end
 
     def debug?
