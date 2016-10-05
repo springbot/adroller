c1e008b1166f5c1c426a964fbd90448fd0a8d6d0
Nikolas Davis <nikolasliamdavis@gmail.com>
2016-10-05 14:43:53 -0400

    SB-7806: Add spec

---
+++ spec/lib/adroll/uhura/userlists.spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe AdRoll::Uhura::Userlists do
+  let!(:basic_auth) { "#{AdRoll.user_name}:#{AdRoll.password}" }
+  let!(:base_uri) { "https://app.adroll.com/uhura/v1/userlists" }
+
+  subject { described_class }
+
+
+  describe '::ad' do
+    let!(:request_uri) { "#{base_uri}/userlists" }
+    let!(:params) do
+      { ad_eids: 'AD123' }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.ad(ad_eids: 'AD123')
+      expect(WebMock).to have_requested(:get, request_uri).with(query: params)
+    end
+  end
+
+  describe '::adgroup' do
+    let!(:request_uri) { "#{base_uri}/adgroup" }
+    let!(:params) do
+      { ad_eids: 'AD123' }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.adgroup(ad_eids: 'AD123')
+      expect(WebMock).to have_requested(:get, request_uri).with(query: params)
+    end
+  end
+
+  describe '::advertisable' do
+    let!(:request_uri) { "#{base_uri}/advertisable" }
+    let!(:params) do
+      { ad_eids: 'AD123' }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.advertisable(ad_eids: 'AD123')
+      expect(WebMock).to have_requested(:get, request_uri).with(query: params)
+    end
+  end
+
+  describe '::segment' do
+    let!(:request_uri) { "#{base_uri}/segment" }
+    let!(:params) do
+      { ad_eids: 'AD123' }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.segment(ad_eids: 'AD123')
+      expect(WebMock).to have_requested(:get, request_uri).with(query: params)
+    end
+  end
+end
