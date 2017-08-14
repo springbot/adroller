ac225342c1afa12492b5f1a3b5c178080edf162d
Chris Oh <seokkyu125@gmail.com>
2017-08-14 16:41:32 -0400

    SB-13288: update facebook authentication to take different request methods

--- lib/adroll/api/facebook.rb
+++ lib/adroll/api/facebook.rb
@@ -5,8 +5,8 @@ module AdRoll
     class Facebook < AdRoll::Api::Service
       WHITELIST_PARAMS = [:page_url, :account_id, :advertiser_eid].freeze
 
-      def fb_page_url(params)
-        call_api(:post, __method__, sanitize_params(params))
+      def fb_page_url(params, method = :get)
+        call_api(method, __method__, sanitize_params(params))
       end
 
       def instagram_account(params)
