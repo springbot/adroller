08d0389ab99c21ecd1a8967e1bcc0832cecc54f1
Chris Oh <seokkyu125@gmail.com>
2018-02-15 11:57:14 -0500

    SB-16439: fix specs and up version

--- lib/adroll/api/dynamic_template_capability_description.rb
+++ lib/adroll/api/dynamic_template_capability_description.rb
@@ -1,8 +1,18 @@
 module AdRoll
   module Api
     class DynamicTemplateCapabilityDescription < AdRoll::Api::Service
-      def get_all
-        call_api(:get, __method__, [])
+      WHITELIST_PARAMS = [].freeze
+
+      def get_all(params = {})
+        call_api(:get, __method__, sanitize_params(params))
+      end
+
+      private
+
+      def sanitize_params(params)
+        params.reject do |key, value|
+          !WHITELIST_PARAMS.include?(key) || value.nil?
+        end
       end
     end
   end
