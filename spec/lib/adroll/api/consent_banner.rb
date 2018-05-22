2bb8e442157f2b930223c1e64edd81288464769e
Matthew Cianciolo <pickledyamsman@gmail.com>
2018-05-22 17:08:16 -0400

    SB2-2183: Adds Consent Banner class

---
+++ spec/lib/adroll/api/consent_banner.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe AdRoll::Api::ConsentBanner do
+  let!(:base_uri) { 'https://services.adroll.com/api/v1/consent_banner' }
+
+  subject { described_class }
+
+  before(:each) do
+    request_uri << "?apikey=#{AdRoll.api_key}"
+  end
+
+  describe '::get' do
+    let(:request_uri) { "#{base_uri}/update" }
+    let(:params) do
+      {
+        advertisable: 'abc123',
+        state: 'adroll'
+      }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.update(params)
+      expect(WebMock).to have_requested(:post, request_uri)
+        .with(query: params)
+    end
+  end
+end
