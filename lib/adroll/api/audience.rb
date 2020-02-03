d390cfe25049149d1a4e4c8409419b9d9d4c55ac
kuhreez <seokkyu125@gmail.com>
2020-02-03 12:39:24 -0500

    SB2-14041: create edit segments audience

--- lib/adroll/api/audience.rb
+++ lib/adroll/api/audience.rb
@@ -1,10 +1,18 @@
 module AdRoll
   module Api
     class Audience < AdRoll::Api::Service
-      WHITELIST_PARAMS = [:first_party, :third_party, :source, :categorized].freeze
+      WHITELIST_PARAMS = [:first_party, :third_party, :source, :categorized, :advertiser_id,
+                          :name, :type, :source_segment_eid, :duration, :size, :countries,
+                          :allow_international_seeds, :attributes, :conversion_value,
+                          :data, :is_conversion].freeze
 
       def user_attribute_names(advertisable_eid, params)
         call_api(:get, "#{__method__}/#{advertisable_eid}", sanitize_params(params))
+      end
+
+      def segments(params, segment_id = nil)
+        endpoint = segment_id.present? ? "#{__method__}/#{segment_id}" : __method__
+        call_api(:post, endpoint, sanitize_params(params))
       end
 
       private
