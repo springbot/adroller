520816e0bb81f450b9bfb99289b5c8154efbcd19
Chris Oh <seokkyu125@gmail.com>
2020-04-22 11:30:21 -0400

    SB2-14949: add valid lal segments endpoint

--- lib/adroll/api/audience.rb
+++ lib/adroll/api/audience.rb
@@ -1,10 +1,10 @@
 module AdRoll
   module Api
     class Audience < AdRoll::Api::Service
-      WHITELIST_PARAMS = [:first_party, :third_party, :source, :categorized, :advertiser_id,
-                          :name, :type, :source_segment_eid, :duration, :size, :countries,
-                          :allow_international_seeds, :attributes, :conversion_value,
-                          :data, :is_conversion].freeze
+      WHITELIST_PARAMS = %i[first_party third_party source categorized advertiser_id
+                            name type source_segment_eid duration size countries
+                            allow_international_seeds attributes conversion_value
+                            data is_conversion].freeze
 
       def user_attribute_names(advertisable_eid, params)
         call_api(:get, "#{__method__}/#{advertisable_eid}", sanitize_params(params))
@@ -13,6 +13,10 @@ module AdRoll
       def segments(params, segment_id = nil)
         endpoint = segment_id.present? ? "#{__method__}/#{segment_id}" : __method__
         call_api(:post, endpoint, sanitize_params(params).to_json)
+      end
+
+      def crosschannel_lal_segments(params)
+        call_api(:get, "#{__method__}/valid-segments", sanitize_params(params))
       end
 
       private
